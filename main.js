2234473ed8d5b7016218653e1c80027cdb3e2de7
Nemanja <nemanjavukojevic44@gmail.com>
2023-11-04 18:00:59 -0400

    Added functionality to make new grids with custom amount of fields

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   for (let i = 0; i < 16; i++) {
     for (let k = 0; k < 16; k++) {
       let square = document.createElement("div");
-      square.className = "squares";
+      square.className = "squares brisati";
       container.appendChild(square);
     }
   }
@@ -18,5 +18,49 @@ document.addEventListener("DOMContentLoaded", function () {
     x.addEventListener("click", () => {
       x.style.background = "white";
     });
+  });
+
+  function newGridLoader(number) {
+    const q = document.querySelectorAll(".brisati");
+    q.forEach((x) => {
+      x.style.display = "none";
+    });
+
+    for (let i = 0; i < number; i++) {
+      for (let k = 0; k < number; k++) {
+        let square = document.createElement("div");
+        square.className = "brisati";
+        let contHeight = container.clientHeight;
+        let contWidth = container.clientWidth;
+        let squareSide = Math.max((contHeight * 0.999) / number);
+
+        // square.style.height = Math.min((contHeight * 0.999) / number);
+        // square.style.width = Math.min((contWidth * 0.999) / number);
+        square.style.width = squareSide + "px";
+        square.style.height = squareSide + "px";
+        square.style.border = "0.1px solid black";
+        square.style.backgroundColor = "white";
+        container.appendChild(square);
+      }
+    }
+    const squares = document.querySelectorAll(".brisati");
+    squares.forEach((x) => {
+      x.addEventListener("mouseover", () => {
+        x.style.background = "black";
+        x.style.border = "0.1px solid white";
+      });
+      x.addEventListener("click", () => {
+        x.style.background = "white";
+        x.style.border = "0.1px solid black";
+      });
+    });
+  }
+
+  const gridBtn = document.querySelector("#createGridButton");
+  gridBtn.addEventListener("click", () => {
+    let input = document.getElementById("number").value;
+    input = parseInt(input);
+
+    newGridLoader(input);
   });
 });
